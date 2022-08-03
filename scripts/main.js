790b99fc53a4d6f99751a1a8c459f265f505c3b4
Alexandre Sousa Silva <Alexandre.Sousa@gigroup.com>
2022-08-03 01:39:45 -0300

    Correção das funções que sorteiam número, caractere especial e letras

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,47 @@
-// var senha = 
+var opcoesSenha;
+var senha = [];
 
-function pswGenerate(){
+function geraSenha() {
     let tamanhoSenha = document.getElementById('pswLeght').value;
-    let qntSenhas = document.getElementById('pswQtd').value;  
+    let qntSenhas = document.getElementById('pswQtd').value;
 
-    let senha = [];
     senha.length = tamanhoSenha;
-    
-    for(i = 0; i < senha.length; i++){
-        console.log(senha.length);
-        // Resolver bug com o loop infinito
+
+    for (let i = 0; i < senha.length; i++) {
         senha[i] = sorteiaLetra();
     }
+
+    verificaCheckbox();
+    algoritmoSenha();
+
+    // switch (opcoesSenha) {
+    //     case 0:
+    //         for (let i = 0; i < senha.length; i++) {
+    //             senha[i] = sorteiaLetra();
+    //         }
+    //         break;
+
+    //     case 1:
+    //         for (let i = 0; i < senha.length; i++) {
+    //             senha[i] = sorteiaLetra();
+    //             senha[i] = senha[i].toUpperCase();
+    //         }
+
+    //     case 2:
+    //         console.log("Opção 2");
+    //         break;
+
+    //     case 3:
+    //         console.log("Opção 3");
+    //         break;
+
+    //     default:
+    //         console.log("Opção inválida");
+    //         break;
+
+    // }
+    console.log(senha);
+
 
 
     // // Converte para letras maiúsculas
@@ -19,49 +49,107 @@ function pswGenerate(){
     //     arrLetras[i] = arrLetras[i].toUpperCase();
     // }
 
-    // Sorteia letras
-    for(i = 0; i < senha.length; i++){
-        console.log(i)
-        let caractere = sorteiaNumero();
-        senha[i] = caractere;
-    }
+    // // Sorteia letras
+    // for(i = 0; i < senha.length; i++){
+    //     let caractere = Math.floor(Math.random() * arrLetras.length);
+    //     senha[i] = arrLetras[caractere];
+    // }
 
     // console.log(sorteiaCaractereEspecial());
     // console.log(sorteiaNúmero());
 
 
-
-    console.log(senha);
+    // console.log(senha);
 
 
     // let arrSenhas = [];
     // arrSenhas.length = qntSenhas;
 }
 
-function sorteiaCaractereEspecial(){
+function sorteiaCaractereEspecial() {
     let arrCaracteresEspeciais = ' !#$%&@'.split('');
-    for(i = 0; i < 1; i++){
-        let caractere = Math.floor(Math.random() * arrCaracteresEspeciais.length);
+    for (let i = 0; i < 1; i++) {
+        var caractere = Math.floor(Math.random() * arrCaracteresEspeciais.length);
         caractere = arrCaracteresEspeciais[caractere];
     }
     return caractere;
 }
 
-function sorteiaNumero(){
+function sorteiaNúmero() {
     let arrNumeros = '0123456789'.split('');
-    for(i = 0; i < 1; i++){
-        let caractere = Math.floor(Math.random() * arrNumeros.length);
+    for (let i = 0; i < 1; i++) {
+        var caractere = Math.floor(Math.random() * arrNumeros.length);
         caractere = arrNumeros[caractere];
     }
     return caractere;
 }
 
-function sorteiaLetra(){
+function sorteiaLetra() {
     let arrLetras = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    
-    for(i = 0; i < 1; i++){
-        let caractere = Math.floor(Math.random() * arrLetras.length);
+    for (let i = 0; i < 1; i++) {
+        var caractere = Math.floor(Math.random() * arrLetras.length);
         caractere = arrLetras[caractere];
     }
     return caractere;
+}
+
+function verificaCheckbox() {
+    let incluirCaractereEspecial = document.getElementById("special");
+    let incluirMaiusculas = document.getElementById("number");
+    let incluirNumeros = document.getElementById("upperCase");
+
+    opcoesSenha = 0;
+
+    if (incluirCaractereEspecial.checked) {
+        opcoesSenha++;
+    }
+
+    if (incluirMaiusculas.checked) {
+        opcoesSenha++;
+    }
+
+    if (incluirNumeros.checked) {
+        opcoesSenha++;
+    }
+}
+
+function algoritmoSenha(){
+    let x = 0;
+    let y = 0;
+    let z = 0; 
+
+    for(let i = 0; i < opcoesSenha.length; i++){
+
+    }
+
+    switch (opcoesSenha) {
+        case 0:
+            for (let i = 0; i < senha.length; i++) {
+                senha[i] = sorteiaLetra();
+            }
+            break;
+
+        case 1:
+            for (let i = 0; i < senha.length; i++) {
+                while ((x + y) != senha.length){
+                    x = Math.floor(Math.random() * 3) + 1;
+                    y = Math.floor(Math.random() * 20) + 1;
+
+                }
+                console.log(`x = ${x} e y ${y}`);
+            }
+
+        case 2:
+            console.log("Opção 2");
+            break;
+
+        case 3:
+            console.log("Opção 3");
+            break;
+
+        default:
+            console.log("Opção inválida");
+            break;
+
+    }
 }
